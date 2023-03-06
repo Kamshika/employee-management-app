39a11c5200ad9d99eeb38cb3170671ab3195ce75
Kamshika_TK <kamshika236@gmail.com>
2023-03-06 13:57:02 +0530

    implement header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <p class="navbar-brand mx-auto my-auto" href="#">
+        Employment Management
+      </p>
+    </nav>
+  );
+};
+
+export default Header;
